fb6f73a97d487fa525bc02ca3151d56b6cd9dc31
Morena Mphuthi <104318051+more-mr@users.noreply.github.com>
2025-03-11 14:27:04 +0200

    Update index.js
    test

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+
+
+await getData()
+
 var today = new Date();
 var opt = { year: "numeric" };
 var year = today.toLocaleDateString("en-US", opt);
 
 document.querySelectorAll("#year")[0].innerHTML =  year;
+
+
+
+async function getData() {
+
+document.getElementsByClassName("hero__description")[0].innerHTML= "<button id="testB"> Click Me! </button>"
+
+document.getElementById("testB").addEventListener("click", function(){
+console.log("fuck you");
+})
+
+
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
